bbf579a8b7519b954a536c34217201e6b02ba2c7
victor2996 <vicctor96@ukr.net>
2021-01-24 21:07:32 +0200

    Finish

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-// function menu() {
-//     document.querySelector('.menu').classList.toggle('open');
-//     document.querySelector('.navbar__burger').classList.toggle('open');
-// }
-
 $(document).ready(function () {
     $('.navbar__burger').click(function (event) {
-        $('.menu').toggleClass('open');
-        $('.navbar__burger').toggleClass('open');
+        $('.menu, .navbar__burger').toggleClass('open');
+        $('body').toggleClass('lock');
     });
 });
